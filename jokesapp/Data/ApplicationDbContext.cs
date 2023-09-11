6c80289a2c4e2d3a049ddc3f766a7b2b0af090c1
Noa Margeta <nmargeta@gmail.com>
2023-09-11 19:37:57 +0200

    Add jokes and users table

--- jokesapp/Data/ApplicationDbContext.cs
+++ jokesapp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using jokesapp.Models;
 
 namespace jokesapp.Data;
 
@@ -9,4 +10,5 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<jokesapp.Models.Joke> Joke { get; set; } = default!;
 }
--- jokesapp/Data/Migrations/20230911172858_AddJokes.Designer.cs
+++ jokesapp/Data/Migrations/20230911172858_AddJokes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using jokesapp.Data;
 
@@ -10,9 +11,11 @@ using jokesapp.Data;
 namespace jokesapp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230911172858_AddJokes")]
+    partial class AddJokes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.10");
